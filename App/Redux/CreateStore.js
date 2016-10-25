810acf2c794d4eec69ef1ba24990162f59ee29fd
Daniel Suchy <suchydan@gmail.com>
2016-10-25 15:30:34 +0200

    Redux files typo fixes

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { autoRehydrate } from 'redux-persist'
 import createLogger from 'redux-logger'
-import Config from '../Config/DebugSettings'
 import createSagaMiddleware from 'redux-saga'
 import R from 'ramda'
+import Config from '../Config/DebugSettings'
 import RehydrationServices from '../Services/RehydrationServices'
 import ReduxPersist from '../Config/ReduxPersist'
 import { StartupTypes } from './StartupRedux'
@@ -29,7 +29,9 @@ export default (rootReducer, rootSaga) => {
     // silence these saga-based messages
     // create the logger
     const logger = createLogger({
-      predicate: (getState, { type }) => USE_LOGGING && R.not(R.contains(type, SAGA_LOGGING_BLACKLIST))
+      predicate: (getState, { type }) => (
+        USE_LOGGING && R.not(R.contains(type, SAGA_LOGGING_BLACKLIST))
+      )
     })
     middleware.push(logger)
   }
